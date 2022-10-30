085b114dc583ed21728fca807f7e357bff2b42fe
Mamadou-Bobo <diallomamadoubobo999@gmail.com>
2022-10-31 00:39:15 +0800

    adding exception handler

---
+++ src/main/java/com/bobo/tontine/authentication/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,22 @@
+package com.bobo.tontine.authentication.security;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Mamadou Bobo on 30/10/2022
+ * @Project Tontine
+ */
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException {
+        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED,"Unauthorized");
+    }
+}
---
+++ src/main/java/com/bobo/tontine/authentication/utils/CustomAccessDeniedHandler.java
@@ -0,0 +1,6 @@
+package com.bobo.tontine.authentication.utils;/**
+  *
+  * @Project Tontine
+  * @author Mamadou Bobo on 30/10/2022
+  */public class CustomAccessDeniedHandler {
+}
